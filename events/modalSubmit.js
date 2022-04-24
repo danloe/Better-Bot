2d25ac96510093aecb42528d963ded7691bbe028
danloe <danloe@users.noreply.github.com>
2022-04-24 21:46:10 +0200

    fix

--- events/modalSubmit.js
+++ events/modalSubmit.js
@@ -2,7 +2,6 @@ const { Formatters } = require('discord.js');
 
 module.exports = {
 	name: 'modalSubmit',
-	once: true,
 	execute(modal) {
 		if (modal.customId === 'modal-customid') {
 			const firstResponse = modal.getTextInputValue('textinput-customid1');
