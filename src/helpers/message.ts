f228d340bcdeafeb762ada621bbd8fbad5644b88
danloe <danloe@users.noreply.github.com>
2022-05-09 10:51:16 +0200

    music implementation

--- src/helpers/message.ts
+++ src/helpers/message.ts
@@ -1,13 +1,15 @@
 import { ColorResolvable, MessageEmbed } from 'discord.js';
 
-export function createEmbed(title: string, message: string, color: ColorResolvable = '#1e81b0') {
+export function createEmbed(title: string, message: string, ephemeral: boolean = false, color: ColorResolvable = '#1e81b0') {
     return {
-        embeds: [new MessageEmbed().setColor(color).setTitle(title).setDescription(message)]
+        embeds: [new MessageEmbed().setColor(color).setTitle(title).setDescription(message)],
+        ephemeral: ephemeral
     };
 }
 
-export function createErrorEmbed(message: string) {
+export function createErrorEmbed(message: string, ephemeral: boolean = true,) {
     return {
-        embeds: [new MessageEmbed().setColor('#951020').setTitle('Error').setDescription(message)]
+        embeds: [new MessageEmbed().setColor('#951020').setTitle('Error').setDescription(message)],
+        ephemeral: ephemeral
     };
 }
