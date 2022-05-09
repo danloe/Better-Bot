c9fdc0c2351ef049e916c93afedac72348589196
danloe <danloe@users.noreply.github.com>
2022-05-09 22:52:38 +0200

    ts fix

--- src/commands/speak/say.ts
+++ src/commands/speak/say.ts
@@ -1,11 +1,11 @@
 import { Command } from '../../interfaces';
-import { CommandInteraction, Message } from 'discord.js';
+import { ButtonInteraction, CommandInteraction, Message } from 'discord.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
 import BetterClient from '../../client';
 
 export const command: Command = {
     data: new SlashCommandBuilder().setName('say').setDescription('Says what you want.'),
-    run: async (client: BetterClient, interaction?: CommandInteraction, message?: Message, args?: string[]) => {
+    run: async (client: BetterClient, interaction?: CommandInteraction | ButtonInteraction, message?: Message, args?: string[]) => {
         if (interaction) {
             interaction!.reply(`${interaction.client.ws.ping}ms ping. 🏓`);
         }
