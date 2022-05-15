3f4120b6a0bdd4139ddce1085e0fe46dbd9c341c
danloe <danloe@users.noreply.github.com>
2022-05-15 15:05:49 +0200

    epic free games

---
+++ src/commands/general/epicfree.ts
@@ -0,0 +1,81 @@
+import { Command } from '../../interfaces';
+import { ButtonInteraction, CommandInteraction, Message, MessageEmbed } from 'discord.js';
+import { SlashCommandBuilder } from '@discordjs/builders';
+import BetterClient from '../../client';
+import { createEmbed, createErrorEmbed, replyDefer, replyInteraction } from '../../helpers';
+import { getGames } from 'epic-free-games';
+
+export const command: Command = {
+    data: new SlashCommandBuilder()
+        .setName('epicfree')
+        .setDescription('Shows this weeks free games available in the epic store.'),
+    run: (
+        client: BetterClient,
+        interaction?: CommandInteraction | ButtonInteraction,
+        message?: Message,
+        args?: string[]
+    ) =>
+        new Promise<void>(async (done, error) => {
+            if (interaction) {
+                try {
+                    if (interaction instanceof ButtonInteraction) return;
+
+                    replyDefer(interaction);
+
+                    const games: any = await getGames('DE', true);
+                    if (games) {
+                        games.currentGames.forEach(async (game) => {
+                            if (game.price.lineOffers) {
+                                let date = new Date(game.promotions.promotionalOffers[0].promotionalOffers[0].endDate);
+                                let endDate = date.toLocaleDateString('de');
+
+                                date = new Date(game.promotions.promotionalOffers[0].promotionalOffers[0].startDate);
+                                let startDate = date.toLocaleDateString('de');
+
+                                let image = '';
+                                game.keyImages.forEach((element) => {
+                                    if (element.type === 'OfferImageWide') {
+                                        image = element.url;
+                                    }
+                                });
+
+                                let embedmsg = new MessageEmbed()
+                                    .setColor('#0078F2')
+                                    .setTitle(game.title)
+                                    .setDescription(game.description)
+                                    .setImage(image)
+                                    .addField('Valid from:', startDate, true)
+                                    .addField('Valid until:', endDate, true)
+                                    .addField('Original Price:', game.price.totalPrice.fmtPrice.originalPrice, true);
+
+                                await interaction.channel.send({ content: ' ', embeds: [embedmsg] });
+                            }
+                            await interaction.editReply({
+                                embeds: [
+                                    new MessageEmbed()
+                                        .setColor('#0078F2')
+                                        .setTitle("This week's free games")
+                                        .setDescription(
+                                            '`The following games are available for free this week in the Epic Games store.`'
+                                        )
+                                        .setURL('https://store.epicgames.com/de/free-games')
+                                ]
+                            });
+                        });
+                    }
+                    done();
+                } catch (err) {
+                    try {
+                        await replyInteraction(
+                            interaction,
+                            createErrorEmbed('🚩 Error showing Epic Games Free Games: `' + err + '`')
+                        );
+                    } catch (err2) {
+                        console.log(err2);
+                    }
+                    console.log(err);
+                    error(err);
+                }
+            }
+        })
+};
