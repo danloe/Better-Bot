0b6d5aef3d5a0fea07d4934a67b29614a3b40518
danloe <danloe@users.noreply.github.com>
2022-05-25 18:23:42 +0200

    closes Use config.json #5

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,3 +1,4 @@
+import { ClientPresenceStatus, ExcludeEnum, PresenceStatusData } from 'discord.js';
 import { ActivityTypes } from 'discord.js/typings/enums';
 import BetterClient from '../client';
 import { Event } from '../interfaces';
@@ -6,8 +7,8 @@ export const event: Event = {
     name: 'ready',
     run: async (client: BetterClient) => {
         client.user!.setPresence({
-            activities: [{ name: '🦄', type: ActivityTypes.WATCHING }],
-            status: 'dnd'
+            activities: [{name: client.config.activityName, type: <ExcludeEnum<typeof ActivityTypes, 'CUSTOM'>>client.config.activityType}],
+            status: <ClientPresenceStatus>client.config.status
         });
 
         console.log(`${client.user!.tag} is ready.`);
