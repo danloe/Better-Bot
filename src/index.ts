23987573727291b2bfbb034c912a6a12c5f8ea8d
danloe <danloe@users.noreply.github.com>
2022-05-29 16:39:27 +0200

    key checks

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk';
 import { Intents } from 'discord.js';
 import dotenv from 'dotenv';
 import BotterinoClient from './client';
@@ -5,6 +6,10 @@ dotenv.config();
 
 try {
     // CLIENT
+    if (!process.env.BOT_TOKEN || process.env.BOT_TOKEN!.length < 30) {
+        console.log(chalk.red(`Discord Bot Token seems to be missing! Add BOT_TOKEN="YOURTOKEN" to .env file.`));
+        process.exit(9);
+    }
     const client = new BotterinoClient({
         intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES]
     });
